a137956b374bff68c0e0f3a2924618c01b3ed5ac
Igor Menezes <imigormenezes@gmail.com>
2021-09-04 18:34:16 -0300

    wepack optimize

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const slsw = require('serverless-webpack');
 const path = require('path')
-
+const webpack = require('webpack');
 const isLocal = slsw.lib.webpack.isLocal
 
 module.exports = {
+    plugins: [
+      new webpack.optimize.ModuleConcatenationPlugin(),
+    ],
     mode: isLocal ? 'development' : 'production',
     entry: slsw.lib.entries,
     output: {
